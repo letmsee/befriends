ddcf0cff7bb09e7200647a9ffb7170df6976cc0a
anhduongictk54@gmail.com <anhduongictk54@gmail.com@5774c4f0-6658-5fa4-9383-42b6ff78e3d0>
2012-04-07 21:35:52 +0000

    add use case: View Friend List

---
+++ trunk/befriends/src/java/servlet/managefriend/ViewFriendListServlet.java
@@ -0,0 +1,93 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package servlet.managefriend;
+
+import business.Account;
+import data.access.FriendDAO;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import servlet.MyServlet;
+
+/**
+ *
+ * @author duongna
+ */
+public class ViewFriendListServlet extends MyServlet {
+
+    /**
+     * Processes requests for both HTTP
+     * <code>GET</code> and
+     * <code>POST</code> methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        if (!isLoggedin(request, "account")) {
+            requireLogin(request, response, "/login.jsp");
+            return;
+        }
+        
+        // get accountId
+        HttpSession session = request.getSession();
+        Account acc = (Account) session.getAttribute("account");
+        int accountId = acc.getAccountId();
+        
+        // get friend list
+        ArrayList<Account> friendList = FriendDAO.getFriendList(accountId);
+        request.setAttribute("friendList", friendList);
+        gotoPage(request, response, "/view_friend_list.jsp");
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP
+     * <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP
+     * <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+}
